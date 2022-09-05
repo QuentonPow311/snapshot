d5f99fe22bbcc15859685d18baeb79ca0ab4b30f
QuentonPow311 <quenpowell@gmail.com>
2022-09-04 20:01:23 -0400

    update

--- snapshot-tests/src/App.js
+++ snapshot-tests/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import GitHubCard from './GitHubCard';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" title="Logo"alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- snapshot-tests/src/App.test.js
+++ snapshot-tests/src/App.test.js
@@ -1,8 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+fetch.resetMocks()
+
 });
+
+
+
+test('renders github snapshot', () => {
+const tree = renderer.create(<App/>).toJSON()
+expect(tree).toMatchSnapshot()
+});
+
+test('Populates the user profile', async () =>{
+  const EXPECTED_NAME = null
+  const EXPECTED_LOGIN = "TheBackupDrive"
+  const EXPECTED_AVATAR = "My avatar url"
+  const EXPECTED_REPOS = 35
+  
+  fetch.mockResponseOnce(JSON.stringify({ 
+    name: "",
+  avatar_url: "My url",
+  login: "TheBackupDrive",
+public_repos: "a number",
+
+  }))
+// fetch.mockResponseOnce(JSON.stringify({
+//   name: EXPECTED_NAME,
+//   login: EXPECTED_LOGIN,
+//   'avatar_url': EXPECTED_AVATAR,
+//   'public_repos': EXPECTED_REPOS,
+//   }));
+  render(<App />);
+
+  const name = screen.getByTitle("Git Hub Name")
+  const avatar = screen.getByTitle("Git Hub Avatar")
+  const repos = screen.getByTitle("Repositories")
+  
+  expect(name).toBeInTheDocument()
+  expect(avatar).toBeInTheDocument()
+  expect(public_repos).toBeInTheDocument()
+})
+
